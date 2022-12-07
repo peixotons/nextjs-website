2047663e8f51724f40f59e04556c12d3052ca3d4
Gabriel Peixoto <bielpcosta@hotmail.com>
2022-12-07 13:26:30 -0300

    Making it better

---
+++ components/Hero.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, the name's Gabriel Peixoto",
+      "Guy-who-loves-games.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#F7ab0a" />
+      </h1>
+    </div>
+  );
+}
